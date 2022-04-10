1620cd53c13446f1771288699c3a9589e7baa414
OmerNavon <103376192+OmerNavon@users.noreply.github.com>
2022-04-11 00:21:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    try:
+        return x / y
+    except ZeroDivisionError:
+        print("cannot divide by 0")
+
+print(""" _____________________
+|  _________________  |
+| |Omer's Claculator| |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|""")
+print("Select operation:")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        next_calculation = input("Another calculation? (yes/no): ")
+        if next_calculation == "no":
+            print("Thank you for using my first python calculator :) @OmerNavon on Github")
+            break
+
+    else:
+        print("Invalid Input")
